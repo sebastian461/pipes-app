c4d8153aab746dd03b17f6582ae239b144bd085c
sebastian461 <sebastiano-461@hotmail.com>
2023-08-31 17:32:16 -0500

    Lazy load
    Creation of the components for the pages to use, as well as a lazy loaded routes directory in the main routes directory

---
+++ src/app/products/products-routing.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { BasicsPageComponent } from './pages/basics-page/basics-page.component';
+import { NumbersPageComponent } from './pages/numbers-page/numbers-page.component';
+import { UncommonPageComponent } from './pages/uncommon-page/uncommon-page.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: BasicsPageComponent,
+  },
+  {
+    title: 'Números',
+    path: 'numbers',
+    component: NumbersPageComponent,
+  },
+  {
+    title: 'No comunes',
+    path: 'uncommon',
+    component: UncommonPageComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class ProductsRoutingModule {}
